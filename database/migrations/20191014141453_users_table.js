c2cb4a3dfd06261878b50077d2b5dca8e9d794bf
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-10-14 15:52:49 -0500

    setup migration and db config files

--- database/migrations/20191014141453_users_table.js
+++ database/migrations/20191014141453_users_table.js
@@ -1,8 +1,22 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+	return knex.schema.createTable('users', tbl => {
+		tbl
+			.increments()
+		
+		tbl 
+			.string('username', 56)
+			.notNullable()
+			.unique()
+		
+		tbl
+			.string('password')
+			.notNullable()
+
+
+	})
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+	return knex.schema.dropTableIfExsists('users')
 };
